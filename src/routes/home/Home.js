083c12e266de68dddd42230675373cc5796d5784
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-11-08 15:15:54 +0300

    moon placed

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -11,6 +11,10 @@ function Home() {
           src={require(`../../assets/png/PLANET TIM.png`)}
           className="first-planet-left"
         />
+        <img
+          src={require(`../../assets/png/PLANET DO.png`)}
+          className="first-planet-right"
+        />
         <header>
           <h6>welcome to</h6>
           <img src={require(`../../assets/png/Group 1000002545.png`)} alt="" />
@@ -19,10 +23,18 @@ function Home() {
           <img src={require(`../../assets/png/Vector.png`)} alt="" />
           <p>scroll down to explore</p>
         </div>
-      <img src={require(`../../assets/png/Vector (3).png`)} alt="" className="vector-3" />
+        <img
+          src={require(`../../assets/png/Vector (3).png`)}
+          alt=""
+          className="vector-3"
+        />
       </div>
       <div className="home-second">
-        <img src={require(`../../assets/png/Vector (1).png`)} alt="" className="vector-1"/>
+        <img
+          src={require(`../../assets/png/Vector (1).png`)}
+          alt=""
+          className="vector-1"
+        />
       </div>
     </div>
   );
