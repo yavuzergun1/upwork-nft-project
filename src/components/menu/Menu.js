b5d225ffb152555d23177fdd4fb788dc890e8cf3
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-11-09 22:48:39 +0300

    menu animation setted

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -2,12 +2,15 @@ import React, { useState } from "react";
 import "./Menu.scss";
 function Menu({ showMenu, setShowMenu }) {
   return (
-    <div className={showMenu ? "menu-container" : "none"}>
-      <img
-        src={require(`../../assets/png/x mark bear 1.png`)}
-        className="nav-menu"
-        onClick={() => setShowMenu(!showMenu)}
-      />
+    <div className={showMenu ? "menu-container" : "menu-none"}>
+      <div className="menu-buttons">
+        <img src={require(`../../assets/png/mute bear.png`)} className="menu-mute" />
+        <img
+          src={require(`../../assets/png/x mark bear 1.png`)}
+          className="menu-close"
+          onClick={() => setShowMenu(!showMenu)}
+        />
+      </div>
     </div>
   );
 }
