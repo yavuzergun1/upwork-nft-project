231f8ea633c0b03d9c942cf77ce7f473c9bf4166
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-11-13 15:48:21 +0300

    swiper done

--- src/components/bear-cards-swiper/BearCardsSwiper.js
+++ src/components/bear-cards-swiper/BearCardsSwiper.js
@@ -1,4 +1,4 @@
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
@@ -14,47 +14,53 @@ export default () => {
    
 
   return (
-      <Swiper
-        // install Swiper modules
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={0}
-        slidesPerView={1}
-        navigation
-        // onSwiper={(swiper) => console.log(swiper)}
-        // onSlideChange={() => console.log("slide change")}
-        className="bearCards-swiper"
-      >
-        <SwiperSlide className="bearCards-slide">
-          <img
-            src={require(`../../assets/png/Group 1000002593.png`)}
-            className="bearCards-cards"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="bearCards-slide">
-          <img
-            src={require(`../../assets/png/Group 1000002594.png`)}
-            className="bearCards-cards"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="bearCards-slide">
-          <img
-            src={require(`../../assets/png/Group 1000002588.png`)}
-            className="bearCards-cards"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="bearCards-slide">
-          <img
-            src={require(`../../assets/png/Group 1000002590.png`)}
-            className="bearCards-cards"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="bearCards-slide">
-          <img
-            src={require(`../../assets/png/Group 1000002592.png`)}
-            className="bearCards-cards"
-          />
-        
-        </SwiperSlide>
-      </Swiper>
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+      spaceBetween={0}
+      slidesPerView={1}
+      navigation
+      // onSwiper={(swiper) => console.log(swiper)}
+      // onSlideChange={() => console.log("slide change")}
+      className="bearCards-swiper"
+      autoplay={{
+        delay: 1000,
+        pauseOnMouseEnter: "true",
+        disableOnInteraction: false,
+      }}
+      loop
+      speed={3000}
+    >
+      <SwiperSlide className="bearCards-slide">
+        <img
+          src={require(`../../assets/png/Group 1000002593.png`)}
+          className="bearCards-cards"
+        />
+      </SwiperSlide>
+      <SwiperSlide className="bearCards-slide">
+        <img
+          src={require(`../../assets/png/Group 1000002594.png`)}
+          className="bearCards-cards"
+        />
+      </SwiperSlide>
+      <SwiperSlide className="bearCards-slide">
+        <img
+          src={require(`../../assets/png/Group 1000002588.png`)}
+          className="bearCards-cards"
+        />
+      </SwiperSlide>
+      <SwiperSlide className="bearCards-slide">
+        <img
+          src={require(`../../assets/png/Group 1000002590.png`)}
+          className="bearCards-cards"
+        />
+      </SwiperSlide>
+      <SwiperSlide className="bearCards-slide">
+        <img
+          src={require(`../../assets/png/Group 1000002592.png`)}
+          className="bearCards-cards"
+        />
+      </SwiperSlide>
+    </Swiper>
   );
 };
